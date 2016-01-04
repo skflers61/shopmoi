8abe52da6913f21c2004126179205c21318cf276
adem <adem@192.168.1.93>
2016-01-04 17:28:46 +0100

    projet (accueil site, page produit)

---
+++ src/SM/SiteBundle/Entity/Produit.php
@@ -0,0 +1,227 @@
+<?php
+
+namespace SM\SiteBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Produit
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="SM\SiteBundle\Entity\ProduitRepository")
+ */
+class Produit
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="couleur", type="string", length=50, nullable=true)
+     */
+    private $couleur;
+    
+    /**
+    * @ORM\ManyToOne(targetEntity="SM\SiteBundle\Entity\Produit", cascade={"persist"})
+    * @ORM\JoinColumn(nullable=false)
+    */
+    private $marque;
+    
+    /**
+    * @ORM\ManyToOne(targetEntity="SM\SiteBundle\Entity\Produit", cascade={"persist"})
+    * @ORM\JoinColumn(nullable=false)
+    */
+    private $categorie;
+    
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="prix", type="float")
+     */
+    private $prix;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="modele", type="string", length=255)
+     */
+    private $modele;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="qte_en_stock", type="integer")
+     */
+    private $qteEnStock;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="nb_vente", type="integer")
+     */
+    private $nbVente;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Produit
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+
+
+    /**
+     * Set prix
+     *
+     * @param float $prix
+     * @return Produit
+     */
+    public function setPrix($prix)
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    /**
+     * Get prix
+     *
+     * @return float 
+     */
+    public function getPrix()
+    {
+        return $this->prix;
+    }
+
+    /**
+     * Set modele
+     *
+     * @param string $modele
+     * @return Produit
+     */
+    public function setModele($modele)
+    {
+        $this->modele = $modele;
+
+        return $this;
+    }
+
+    /**
+     * Get modele
+     *
+     * @return string 
+     */
+    public function getModele()
+    {
+        return $this->modele;
+    }
+
+    /**
+     * Set qteEnStock
+     *
+     * @param integer $qteEnStock
+     * @return Produit
+     */
+    public function setQteEnStock($qteEnStock)
+    {
+        $this->qteEnStock = $qteEnStock;
+
+        return $this;
+    }
+
+    /**
+     * Get qteEnStock
+     *
+     * @return integer 
+     */
+    public function getQteEnStock()
+    {
+        return $this->qteEnStock;
+    }
+
+    /**
+     * Set nbVente
+     *
+     * @param integer $nbVente
+     * @return Produit
+     */
+    public function setNbVente($nbVente)
+    {
+        $this->nbVente = $nbVente;
+
+        return $this;
+    }
+
+    /**
+     * Get nbVente
+     *
+     * @return integer 
+     */
+    public function getNbVente()
+    {
+        return $this->nbVente;
+    }
+
+    /**
+     * Set couleur
+     *
+     * @param string $couleur
+     * @return Produit
+     */
+    public function setCouleur($couleur)
+    {
+        $this->couleur = $couleur;
+
+        return $this;
+    }
+
+    /**
+     * Get couleur
+     *
+     * @return string 
+     */
+    public function getCouleur()
+    {
+        return $this->couleur;
+    }
+}
